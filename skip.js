40f2ba898d3d0f4a9de8b7bd233059f5ceda8fd6
Graham Goudeau <grahamgoudeau@gmail.com>
2016-07-20 22:01:36 -0400

    Added additional check when redirecting

--- skip.js
+++ skip.js
@@ -2,7 +2,7 @@ function checkForRedirect(url) {
     var waitTimer = document.getElementById('sm_dl_wait'),
         nextUrl;
 
-    if (waitTimer !== null) {
+    if (waitTimer !== null && document.domain === 'imslp.org') {
         nextUrl = waitTimer.getAttribute('data-id');
         window.location.href = nextUrl;
     }
